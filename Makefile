676acde5fafbbb00de7edaefa4788fc268f3c7d2
Daniel Standage <daniel.standage@gmail.com>
2025-09-27 16:45:48 -0400

    Setup

--- Makefile
+++ Makefile
@@ -8,3 +8,8 @@ format:
 
 test:
 	pytest
+
+hooks:
+	echo 'set -eo pipefail' > .git/hooks/pre-commit
+	echo 'make style' >> .git/hooks/pre-commit
+	chmod 755 .git/hooks/pre-commit
--- src/orgphotos/tests/test_suite.py
+++ src/orgphotos/tests/test_suite.py
@@ -1,6 +1,12 @@
 # This file is part of orgphotos (https://github.com/standage/orgphotos).
 # (c) Daniel Standage, 2025.
 
+from orgphotos import cli
+import pytest
 
-def test_placeholder():
-    pass
+
+def test_usage(capsys):
+    with pytest.raises(SystemExit):
+        cli.driver(["--help"])
+    terminal = capsys.readouterr()
+    assert "-d, --dryrun" in terminal.out
